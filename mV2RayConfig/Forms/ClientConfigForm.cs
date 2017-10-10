3c1081e535e658f3ef0d97d623e7af8a30ed32d8
Milkey Tan <819774989@qq.com>
2017-10-10 11:07:44 +0800

    less using

--- mV2RayConfig/Forms/ClientConfigForm.cs
+++ mV2RayConfig/Forms/ClientConfigForm.cs
@@ -1,12 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using static mV2RayConfig.Config;
 
